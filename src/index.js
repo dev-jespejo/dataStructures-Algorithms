5c82838e235276725dd40d589d5053a9c31cda42
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-06 10:55:08 -0300

    Implementing Array

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const checkCommonItems = require('./containCommonItem');
+const strings = require('./01_arrays/arrays');
 
 const array1 = ['a', 'b', 'c', 'x'];
 const array2 = ['z', 'y', 'x'];
 
 const result = checkCommonItems(array1, array2);
-console.log(`¿Tienen elementos comunes?: ${result}`);
