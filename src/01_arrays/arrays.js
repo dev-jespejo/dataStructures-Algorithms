7dc8e2c5fe560732525204174e1982e3622c222d
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-06 09:57:58 -0300

    Arrays introduction

---
+++ src/01_arrays/arrays.js
@@ -0,0 +1,22 @@
+// arrays
+let strings = ['a', 'b', 'c', 'd'];
+// 4*4 = 16 bytes of storage
+
+strings[2]; // O(1)
+
+//push
+strings.push('e'); // O(1)
+
+//pop
+strings.pop();
+strings.pop(); // O(1)
+
+//unshift
+strings.unshift('x'); // O(n)
+
+// splice
+strings.splice(2, 0, 'alien'); // O(n/2) = O(n)
+
+console.log(strings);
+
+
